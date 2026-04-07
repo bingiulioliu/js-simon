d8835b2d8d2dd3c8f0efea2d821b272441404b83
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-07 16:29:37 +0200

    ADD array con numeri casuali nel form

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,28 @@
-// Funzione per mostrare il form e nascondere il bottone di inizializzazione
+// Funzione per:
+// mostrare il form  
+// nascondere il bottone di inizializzazione
+// Generare numeri random negli input
 function btnInit() {
-  // Mostro il form
+    // Mostro il form
     formGioco.classList.remove('d-none');
-  // Nascondo il bottone Gioca ora
+    // Nascondo il bottone Gioca ora
     btnLetsPlay.classList.add('d-none');
-  // Alternativa senza CSS
-  // btnRimozione.style.display = 'none';
+    // Alternativa senza CSS
+    // btnRimozione.style.display = 'none';
+
+    // Per ogni input, inserisco numero random 1-50
+    // La freccia => deve stare dentro le parentesi del forEach.
+    inputs.forEach(input => {
+        let numero;
+        // Continua a generare finché non ne trovi uno NON presente nell'array
+        do {
+            numero = Math.floor(Math.random() * 50) + 1;
+        } while (numeriEstratti.includes(numero));
+
+        numeriEstratti.push(numero);
+        input.value = numero;
+        console.log(numeriEstratti);
+    });
 }
+
 
--- js/script.js
+++ js/script.js
@@ -3,7 +3,14 @@
 const btnLetsPlay = document.querySelector('#letsPlay');
 const formGioco = document.querySelector('#answers-form');
 
+// Variabile per gli input
+const inputs = document.querySelectorAll('#input-group input');
 
+// Inizia a giocare allo scadere del timer o alla pressione del brn Conferma
 const btnConferma = document.querySelector('#btnConferma');
 
+// Per registrare i numeri estratti
+const numeriEstratti = [];
+
 btnLetsPlay.addEventListener('click', btnInit);
+
