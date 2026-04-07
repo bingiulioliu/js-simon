70e1397ed3f98daf159b6899b8381673f3a97afc
Bin Giulio Liu <bingiulio.liu@gmail.com>
2026-04-07 15:52:05 +0200

    ADD start btn

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+// Funzione per mostrare il form e nascondere il bottone di inizializzazione
+function btnInit() {
+  // Mostro il form
+    formGioco.classList.remove('d-none');
+  // Nascondo il bottone Gioca ora
+    btnLetsPlay.classList.add('d-none');
+  // Alternativa senza CSS
+  // btnRimozione.style.display = 'none';
+}
+
---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Bottone per iniziare a giocare:
+// Rimuovere classe d-none e nascondere il bottone
+const btnLetsPlay = document.querySelector('#letsPlay');
+const formGioco = document.querySelector('#answers-form');
+
+
+const btnConferma = document.querySelector('#btnConferma');
+
+btnLetsPlay.addEventListener('click', btnInit);
